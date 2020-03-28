20460f097c7dcb3b28a597387667be82fd58aa48
Pulkit008 <61957883+Pulkit008@users.noreply.github.com>
2020-03-28 20:31:06 +0530

    Create 01. Classes: Dealing with Complex Numbers.py

---
+++ Python/10. Classes/01. Classes: Dealing with Complex Numbers.py
@@ -0,0 +1,30 @@
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real=real
+        self.imaginary=imaginary
+    def __add__(self, no):
+        return Complex(self.real+no.real,self.imaginary+no.imaginary)
+    def __sub__(self, no):
+        return Complex(self.real-no.real,self.imaginary-no.imaginary)
+    def __mul__(self, no):
+        return Complex(self.real*no.real-self.imaginary*no.imaginary,self.real*no.imaginary+self.imaginary*no.real)
+    def __truediv__(self, no):
+        no1=Complex(no.real,-1*no.imaginary)
+        nr=self.__mul__(no1)
+        dr=no.__mul__(no1)
+        return Complex(nr.real/dr.real,nr.imaginary/dr.real)
+    def mod(self):
+        return Complex((self.real**2+self.imaginary**2)**0.5,0)
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
