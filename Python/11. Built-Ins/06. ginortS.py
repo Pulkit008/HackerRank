7931e24074154e8f8c1898fdab04fc75735ed2ce
Pulkit008 <61957883+Pulkit008@users.noreply.github.com>
2020-03-28 20:59:58 +0530

    Update 06. ginortS.py

--- Python/11. Built-Ins/06. ginortS.py
+++ Python/11. Built-Ins/06. ginortS.py
@@ -1,1 +1,16 @@
+# Either
 print(*sorted(input(),key=lambda a: ((1,a) if a.islower() else (2,a)) if a.isalpha() else ((3,a) if int(a)%2==1 else (4,a))),sep='')
+
+# or
+def ck(a):
+    if a.isalpha():
+        if a.islower():
+            return (1,a)
+        else:
+            return (2,a)
+    else:
+        if int(a)%2==1:
+            return (3,a)
+        else:
+            return (4,a)
+print(*sorted(input(),key=ck),sep='')
